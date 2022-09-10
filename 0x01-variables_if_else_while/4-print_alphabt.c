ce7a74323da9c96cc988aab5f744ba5f147765d4
Nwadike Joseph <jcyoung.mjc44@gmail.com>
2022-09-09 21:39:57 -0700

    4-print_alphabt.c committed

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -17,7 +17,9 @@ int main(void)
 {
 
 	char low, e, q;
+	
 	e = 'e';
+	
 	q = 'q';
 
 
@@ -27,7 +29,7 @@ int main(void)
 		if (low != e && low != q)
 			putchar(low);	
 	}
-
+	
 	putchar('\n');	
 	return (0);
 }

b6e95b9552aff5628b1a586383445f748263a803
Nwadike Joseph <jcyoung.mjc44@gmail.com>
2022-09-09 21:20:09 -0700

    4-print_alphabt.c committed

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,48 +1,40 @@
-#include <stdio.h>                                                                                                                                                       
+#include <stdio.h>
 
-#include <stdlib.h>                                                                                                                                                       
+#include <stdlib.h>
 
-#include <time.h>                                                                                                                                                         
+#include <time.h>
 
-                                                                                                                                        /* more headers goes there */                                                                                                                                             
+/* more headers goes there */
 
-                                                                                                                                        
-/* betty style doc for function main goes there */                                                                                                                        
+/* betty style doc for function main goes there */
 
 /**
  * main - Prints the alphabet without q and e.
  *
  * Return: Always 0 (Success)
  */
+int main(void)
 
-int main(void)                                            
-
-{                                                                                                                                                                         
+{
+	
 	char low, e, q;
-
-
-
+	
 	e = 'e';
-
+	
 	q = 'q';
-
-
-
+	
 	for (low = 'a'; low <= 'z'; low++)
-
+	
 	{
-
+		
 		if (low != e && low != q)
 		{
-
 			putchar(low);
-
+		
 		}
-
 	}
-
+	
 	putchar('\n');
-	                                                                                                                                                      
-	return (0);		
-
+	
+	return (0);
 }          

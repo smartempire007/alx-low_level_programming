50554a9823c2db39074080ce61233652d2b2e08f
Nwadike Joseph <jcyoung.mjc44@gmail.com>
2022-09-15 07:32:03 -0700

    new change on 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -25,12 +25,12 @@ void times_table(void)
 			if (n <= 9)
 			{
 				_putchar(32);
-				_putchar(op + 48);
+				_putchar(n + 48);
 			}
 			else
 			{
-				_putchar((op / 10) + 48);
-				_putchar((op % 10) + 48);
+				_putchar((n / 10) + 48);
+				_putchar((n % 10) + 48);
 			}
 		}
 		_putchar('\n');

5ef6f144be56f18a506ebf697915bd76f8f4df0a
Nwadike Joseph <jcyoung.mjc44@gmail.com>
2022-09-15 09:06:41 -0700

    new change on 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,54 +1,53 @@
 #include <stdio.h>
+#define LARGEST 10000000000
 
 /**
- * main - Prints the first 98 Fibonacci numbers
+ * main - main block
  *
- * Return: Always 0.
+ * Description: Find and print the first 98 fib numbers starting with 1 and 2.
+ *
+ * Numbers should be coma and space separated.
+ *
+ * Return: 0
  */
-
 int main(void)
 {
-	int c, boolean, boolean2;
-	long int n1, n2, fn, fn2, n11, n22;
+	unsigned long int fr1 = 0, bk1 = 1, fr2 = 0, bk2 = 2;
 
-	n1 = 1;
-	n2 = 2;
+	unsigned long int hold1, hold2, hold3;
 
-	boolean =  boolean2 = 1;
-	printf("%ld, %ld", n1, n2);
-	for (c = 0; c < 96; c++)
+	int count;
+
+	printf("%lu, %lu, ", bk1, bk2);
+
+	for (count = 2; count < 98; count++)
 	{
-		if (boolean)
+		if (bk1 + bk2 > LARGEST || fr2 > 0 || fr1 > 0)
 		{
-			fn = n1 + n2;
-			printf(", %ld", fn);
-			n1 = n2;
-			n2 = fn;
+			hold1 = (bk1 + bk2) / LARGEST;
+
+			hold2 = (bk1 + bk2) % LARGEST;
+
+			hold3 = fr1 + fr2 + hold1;
+
+			fr1 = fr2, fr2 = hold3;
+
+			bk1 = bk2, bk2 = hold2;
+
+			printf("%lu%010lu", fr2, bk2);
 		}
 		else
 		{
-			if (boolean2)
-			{
-				n11 = n1 % 1000000000;
-				n22 = n2 % 1000000000;
-				n1 = n1 / 1000000000;
-				n2 = n2 / 1000000000;
-				boolean2 = 0;
-			}
-			fn2 = (n11 + n22);
-			fn = n1 + n2 + (fn2 / 1000000000);
-			printf(", %ld", fn);
-			printf("%ld", fn2 % 1000000000);
-			n1 = n2;
-			n11 = n22;
-			n2 = fn;
-			n22 = (fn2 % 1000000000);
+			hold2 = bk1 + bk2;
+
+			bk1 = bk2, bk2 = hold2;
+
+			printf("%lu", bk2);
+
+			if (count != 97)
+
+				printf(", ");
 		}
-		if (((n1 + n2) < 0) && boolean == 1)
-
-			boolean = 0;
-	}
-	printf("\n");
-
-	return (0);
+		printf("\n");
+		return (0);
 }

46954586b085b51f76d6544a9047660c24cee8c1
Nwadike Joseph <jcyoung.mjc44@gmail.com>
2022-09-15 06:40:24 -0700

    new change on 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,20 +3,22 @@
 /**
  * main - Entry point
  *
+ * Description: It prints the word _putchar, followed by a new line.
+ *
  * Return: Always 0
  */
 
 int main(void)
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-	_putchar("\n");
+	char word[9] = "_putchar";
 
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(word[i]);
+	}
+
+	_putchar('\n');
 	return (0);
 }

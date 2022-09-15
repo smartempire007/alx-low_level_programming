ce7973a27450d64a0b8799dcbed486a90a65aadd
Nwadike Joseph <jcyoung.mjc44@gmail.com>
2022-09-14 21:02:16 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+
+/**
+ * jack_bauer - Prints every minute of the day of Jack Bauer
+ *
+ * Return: ...
+ */
+
+void jack_bauer(void)
+{
+	int H, M;
+
+	for (H = 0; H < 24; H++)
+	{
+		for (M = 0; M < 60; M++)
+		{
+			_putchar((H / 10) + '0');
+
+			_putchar((H % 10) + '0');
+
+			_putchar(':');
+
+			_putchar((M / 10) + '0');
+
+			_putchar((M % 10) + '0');
+
+			_putchar('\n');
+		}
+	}
+}

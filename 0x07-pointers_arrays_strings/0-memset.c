d0a6da4e06ed83b67f807231a40fa8eab5f0bdb3
Nwadike Joseph <jcyoung.mjc44@gmail.com>
2022-09-26 10:28:21 -0700

    new change on 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,23 +1,20 @@
-#include "main.h"
+#include <main.h>
 
 /**
- * _memset - fills memory with a constant byte,
- *
- * @s: memory area.
- *
- * @b: constant byte.
- *
- * @n: bytes filled.
- *
- * Return: the pointer to dest.
+ * _memset - Fill memory with a constant byte
+ * @s: memory area to fill
+ * @b: constant byte to fill
+ * @n: bytes of memory area to fill
+ * return: the memory area fill
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int a;
+
 	for (a = 0; a < n; a++)
 	{
 		s[a] = b;
 	}
-	return (s)
+	return (s);
 }

2593b902f129a8a8277ab2110152fa9125163b9b
Nwadike Joseph <jcyoung.mjc44@gmail.com>
2022-10-07 17:27:38 -0700

    fixing 0-malloc_checked.c task

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,17 +2,19 @@
 #include <stdlib.h>
 
 /**
- * malloc_checked - A function that allocates memory using malloc.
- * @b: An unsigned input integer
- * Return: Apointer to allocated memory or NULL if it fails
+ * malloc_checked - allocates memory.
+ * @b: amount of bytes.
+ *
+ * Return: pointer to the allocated memory.
+ * if malloc fails, status value is equal to 98.
  */
 
 void *malloc_checked(unsigned int b)
 {
-	void *s;
-	s = malloc(b);
+	char *p;
+	p = malloc(b);
 
-	if (s == NULL)
+	if (p == NULL)
 		exit(98);
-	return (s);
+	return (p);
 }

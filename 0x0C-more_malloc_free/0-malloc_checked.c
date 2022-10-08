0d01babc1cffdc060fee464d21861adff1dda731
Nwadike Joseph <jcyoung.mjc44@gmail.com>
2022-10-07 17:02:51 -0700

    fixing 0-malloc_checked.c task

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,19 +1,20 @@
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
 
 /**
- * malloc_checked - allocates memory using malloc
- * @b: size of memory to be allocated
+ * malloc_checked - Allocates memory
+ * @b: the size to allocate
  *
- * Return: pointer to the allocated memory (SUCCESS)
- * 98 if insufficient memory was available (FAILURE)
+ * Return: Nothing.
  */
 
 void *malloc_checked(unsigned int b)
 {
 	void *p;
 	p = malloc(b);
-	
+
 	if (p == NULL)
 		exit(98);
 	return (p);
